45b5728991c5a393bdbd4d6ecf551e858be6438f
Andrii Pukh <puschock.andrey@gmail.com>
2023-03-17 10:04:50 +0200

    [NTE-16] clear unnecessary logs

--- src/app/errors/BaseError.js
+++ src/app/errors/BaseError.js
@@ -1,6 +1,5 @@
 class BaseError extends Error {
   constructor(name, description, httpCode, isOperational) {
-    console.log(httpCode, 'Actions log<<<<<');
     super(description);
     Object.setPrototypeOf(this, new.target.prototype);
     this.name = name;
