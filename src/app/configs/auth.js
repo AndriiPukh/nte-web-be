9d59e54d26e96aeb084a4cb4278c2fd44d6a6ca5
Andrii Pukh <puschock.andrey@gmail.com>
2023-03-17 09:14:38 +0200

    [NTE-16] Add redis to github actions

--- src/app/configs/auth.js
+++ src/app/configs/auth.js
@@ -6,7 +6,7 @@ const TWENTY_FOUR_HOURS = '24h';
 dotenv.config();
 module.exports = {
   accessSecret: process.env.ACCESS_TOKEN_PRIVATE_KEY,
-  accessTokenTime: process.env.ACCESS_TOKEN_EXPIRE_MILISECOND || HALF_HOUR,
+  accessTokenTime: process.env.REFRESH_TOKEN_EXPIRE_IN_MILISECOND || HALF_HOUR,
   refreshSecret: process.env.REFRESH_TOKEN_PRIVATE_KEY,
   refreshTokenTime:
     process.env.REFRESH_TOKEN_EXPIRE_IN_MILISECOND || TWENTY_FOUR_HOURS,
